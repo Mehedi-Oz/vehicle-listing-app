9038e46c344feeb0d3df5d1a836a2edfa2f724c7
Muhammad Mehedi Hasan <mehedihasan199908@gmail.com>
2025-09-20 22:15:46 +0600

    updated guest layout, signup, login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,15 @@
-@props(['title' => ''])
+@props(['title' => '', 'footerLinks' => ''])
 
 <x-base-layout :$title>
 
-    @include('layouts.partials.header')
+    <x-layouts.header />
 
     {{ $slot }}
 
     <footer>
-         {{$footerLinks}}
         <a href="">Link 1</a>
         <a href="">Link 2</a>
-
+        {{ $footerLinks }}
     </footer>
 
 </x-base-layout>
