cd6739f4214a23e6857e7a3a3c9a1c196357376e
Muhammad Mehedi Hasan <mehedihasan199908@gmail.com>
2025-09-25 03:26:33 +0600

    updated factories

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,24 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\Car;
-use App\Models\CarFeatures;
-use App\Models\CarImage;
-use App\Models\CarType;
 use App\Models\Maker;
+use App\Models\Model;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(): Factory|View
     {
 
-        $maker = Maker::factory()->create();
-        dd($maker);
+        User::factory()
+            ->has(Car::factory()->count(5), 'favouriteCars')
+            ->create();
 
         return view("home.index");
     }
